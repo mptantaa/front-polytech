35e9d5695b1303df6d927b0be44d845078c002aa
mptantaa <mr.anton3@gmail.com>
2024-01-08 00:44:08 +0300

    3 - Routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import logo from './logo.svg';
-import { Table } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
-import './App.css';
-
-interface User {
-  key: number;
-  name: string;
-  age: number;
-}
-
-const columns: ColumnsType<User> = [
-  {
-    key: 'name',
-    title: 'Name',
-    dataIndex: 'name',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    key: 'age',
-    title: 'Age',
-    dataIndex: 'age',
-  },
-];
-
-const data: User[] = [
-  {
-    key: 0,
-    name: 'Jack',
-    age: 20,
-  },
-  {
-    key: 1,
-    name: 'Herry',
-    age: 22,
-  },
-];
-=======
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -46,20 +6,12 @@ import MainRouter from './app/routes';
 
 const App = () => {
   const [isAuth, setIsAuth] = useState(false);
->>>>>>> Stashed changes
 
-function App() {
   return (
-<<<<<<< Updated upstream
-    <>
-      <Table<User> columns={columns} dataSource={data} />
-    </>
-=======
     <Router>
       <Navbar isAuth={isAuth} setIsAuth={setIsAuth} />
       <MainRouter isAuth={isAuth} />
     </Router>
->>>>>>> Stashed changes
   );
 }
 
