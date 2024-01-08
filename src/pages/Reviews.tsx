db5873c80956bada3dd1e26ec0d5098d83c43b11
mptantaa <mr.anton3@gmail.com>
2024-01-09 00:34:02 +0300

    Добавлены формы
    Алексеев А. А. 221-323
    
    https://disk.yandex.ru/i/Vszc6IaylvPTWg

--- src/pages/Reviews.tsx
+++ src/pages/Reviews.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import FormWithValidation from '../components/FormWithValidation';
 
 const StyledDiv = styled.div`
   padding: 0.75rem;
@@ -11,8 +12,7 @@ export const Reviews = () => {
     return (
         <StyledDiv>
             <h1>Отзывы о сайте</h1>
-            <p><b>Влад: </b> лучший сайт про друга</p>
-            <p><b>Илья:</b> привет!</p>
+            <FormWithValidation />
         </StyledDiv>
     );
 };
